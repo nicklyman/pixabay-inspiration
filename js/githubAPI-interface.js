2f84d06dfda2fc7cdb3ed76beb70c780c36c92e2
Nick Lyman <nick.lyman@gmail.com>
2016-05-27 09:40:37 -0700

    Fix README.md error, print out username to screen from GitHub API AJAX request/response.

--- js/githubAPI-interface.js
+++ js/githubAPI-interface.js
@@ -1,11 +1,27 @@
-var GitHubAPI = require('./../js/githubAPI.js').GitHubAPI;
+var GitHubUser = require('./../js/githubAPI.js').GitHubUser;
 
 $(document).ready(function(){
-  $('#printInput').submit(function(event){
-    event.preventDefault();
-    var printField = $('#userInput').val();
 
-    var newGitHubAPI = new GitHubAPI(printField);
-    console.log(newGitHubAPI.print());
+  var requestedGitUserName = new GitHubUser();
+
+  $('#enteredUserName').submit(function(event){
+    event.preventDefault();
+    var username = $('#userInput').val();
+    $('#userInput').val("");
+
+    var functionCurrentUser = function(response) {
+      $('.showUserName').text("Your requested GitHub user is: " + username);
+    };
+
+    // var functionCurrentUserRepos = function(response) {
+    //   $('.showRepoNamesDescriptions').empty();
+    //   $('.showRepoNamesDescriptions').append("The public GitHub repositories for " + username + "are: ");
+    //   for (var i = 0; i < userRepos.length; i++) {
+    //     $('.showRepoNamesDescriptions').append("<li>" + "GitHub Repository Name: " + )
+    //   }
+    // };
+
+    requestedGitUserName.getUserName(username, functionCurrentUser);
+    // requestedGitUserName.getUserRepos(username, functionCurrentUserRepos);
   });
 });
--- js/githubAPI.js
+++ js/githubAPI.js
@@ -1,9 +1,13 @@
 var apiKey = require('./../.env').apiKey;
 
-exports.getRepos = function(username){
-  $.get('https://api.github.com/users/' + username + '?access_token=' + apiKey).then(function(response){
-    console.log(response);
-  }).fail(function(error){
-    console.log(error.responseJSON.message);
+exports.GitHubUser = function() {
+  this.userRepos = [];
+};
+
+exports.GitHubUser.prototype.getUserName = function(username, showFunction) {
+  $.get('https://api.github.com/users/' + username + '?access_token=' + apiKey).then(function(response) {
+    showFunction(username, response.login);
+  }).fail(function(error) {
+    $('.showUserName').text(error.responseJSON.message);
   });
 };
