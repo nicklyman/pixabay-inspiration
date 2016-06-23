489a59d40555ded3a576deb39c60bfb42c2e445a
themingfu <mwen82@gmail.com>
2016-06-23 11:55:13 -0700

    minor changes to about and home html

--- js/pixabay-interface.js
+++ js/pixabay-interface.js
@@ -4,13 +4,12 @@ $(document).ready(function(){
     console.log("clicked");
     var variable = "urban";
 
-    $.get('https://pixabay.com/api/?key=2801588-27810ab4dfe172ad8ddb203f5&q=' + variable + '&image_type=photo').then(function(response){
-        console.log(response);
-          for (var i = 0; i < response.hits.length; i++) {
-            $("#list").append('<a href="' + response.hits[i].pageURL + '"><div class="photo" style="display:inline-block"><img src=' + response.hits[i].webformatURL + '>' + '</a>');
-          }
-        }).fail(function(error){
-        console.log(error.responseJSON.message);
-        });
+      $.get('https://pixabay.com/api/?key=2801588-27810ab4dfe172ad8ddb203f5&q=' + variable + '&image_type=photo').then(function(response){
+            for (var i = 0; i < response.hits.length; i++) {
+              $("#list").append('<a href="' + response.hits[i].pageURL + '"><div class="photo" style="display:inline-block"><img src=' + response.hits[i].webformatURL + '>' + '</a>');
+            }
+          }).fail(function(error){
+          console.log(error.responseJSON.message);
       });
     });
+  });
