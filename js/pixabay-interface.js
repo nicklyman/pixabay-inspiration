3886777e75e11dd307269c1123e66bbbcea19b32
Nick <nick.lyman@gmail.com>
2016-06-23 15:49:12 -0700

    fix form being cleared causing issues with API call for videos.

--- js/pixabay-interface.js
+++ js/pixabay-interface.js
@@ -5,13 +5,12 @@ $(document).ready(function(){
     $("#list").html('');
 
       $.get('https://pixabay.com/api/?key=2801588-27810ab4dfe172ad8ddb203f5&q=' + user_input + '&image_type=photo').then(function(response){
-        console.log(user_input);
         for (var i = 0; i < response.hits.length; i++) {
           $("#list").append('<a href="' + response.hits[i].pageURL + '"><div class="photo" style="display:inline-block"><img src=' + response.hits[i].webformatURL + '>' + '</div></a>');
         }
         }).fail(function(error){
         console.log(error.responseJSON.message);
       });
-      $('#input').val('');
+      // $('#input').val('');
     });
   });
--- js/pixabay-video-interface.js
+++ js/pixabay-video-interface.js
@@ -6,14 +6,15 @@ $(document).ready(function(){
     var vidPrefix = "https://i.vimeocdn.com/video/"
     $("#video-list").html('');
 
-    $.get('https://pixabay.com/api/videos/?key=2801588-27810ab4dfe172ad8ddb203f5&q=' + variable).then(function(response){
+    $.get('https://pixabay.com/api/videos/?key=2801588-27810ab4dfe172ad8ddb203f5&q=' + user_input).then(function(response){
         console.log(response.hits);
+        console.log(user_input);
           for (var i = 0; i < response.hits.length; i++) {
             $("#video-list").append('<a href="' + response.hits[i].videos.small.url + '"><div class="photo" style="display:inline-block"><img src=' + vidPrefix +  response.hits[i].picture_id + vidPreviewSize + '>' + '</a>');
           }
         }).fail(function(error){
         console.log(error.responseJSON.message);
         });
-        $('#input').val('');
+        // $('#input').val('');
       });
     });
