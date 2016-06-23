da35fc6f6c6b8111c12ee8cf64dc0ab561ce4169
themingfu <mwen82@gmail.com>
2016-06-23 10:58:47 -0700

    set photos to sit inline and center. Made images link to src

--- js/pixabay-interface.js
+++ js/pixabay-interface.js
@@ -2,12 +2,15 @@
 $(document).ready(function(){
   $("#button").click(function(){
     console.log("clicked");
-    var variable = "flower";
+    var variable = "urban";
 
     $.get('https://pixabay.com/api/?key=2801588-27810ab4dfe172ad8ddb203f5&q=' + variable + '&image_type=photo').then(function(response){
         console.log(response);
+          for (var i = 0; i < response.hits.length; i++) {
+            $("#list").append('<a href="' + response.hits[i].pageURL + '"><div class="photo" style="display:inline-block"><img src=' + response.hits[i].webformatURL + '>' + '</a>');
+          }
         }).fail(function(error){
         console.log(error.responseJSON.message);
         });
-      })
-    })
+      });
+    });
