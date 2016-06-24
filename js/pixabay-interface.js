830f2c57b1fc97b89769de5607ca6000e8cc40ca
themingfu <mwen82@gmail.com>
2016-06-23 19:08:52 -0700

    Add .env key

--- js/pixabay-interface.js
+++ js/pixabay-interface.js
@@ -1,10 +1,12 @@
+var apiKey = require('./../.env').apiKey;
+
 $(document).ready(function(){
   $("#user-input").click(function(){
     variable = ($('#input').val());
     var user_input = variable;
     $("#list").html('');
 
-      $.get('https://pixabay.com/api/?key=2801588-27810ab4dfe172ad8ddb203f5&q=' + user_input + '&image_type=photo').then(function(response){
+      $.get('https://pixabay.com/api/?key=' + apiKey + '&q=' + user_input + '&image_type=photo').then(function(response){
         for (var i = 0; i < response.hits.length; i++) {
           $("#list").append('<a href="' + response.hits[i].pageURL + '"><div class="photo" style="display:inline-block"><img src=' + response.hits[i].webformatURL + '>' + '</div></a>');
         }
--- js/pixabay-video-interface.js
+++ js/pixabay-video-interface.js
@@ -1,3 +1,5 @@
+var apiKey = require('./../.env').apiKey;
+
 $(document).ready(function(){
   $("#user-input").click(function(){
     variable = ($('#input').val());
@@ -6,7 +8,7 @@ $(document).ready(function(){
     var vidPrefix = "https://i.vimeocdn.com/video/"
     $("#video-list").html('');
 
-    $.get('https://pixabay.com/api/videos/?key=2801588-27810ab4dfe172ad8ddb203f5&q=' + user_input).then(function(response){
+    $.get('https://pixabay.com/api/videos/?key=' + apiKey + '&q=' + user_input).then(function(response){
         for (var i = 0; i < response.hits.length; i++) {
           $("#video-list").append('<a href="' + response.hits[i].videos.small.url + '"><div class="photo" style="display:inline-block"><img src=' + vidPrefix +  response.hits[i].picture_id + vidPreviewSize + '>' + '</a>');
         }
